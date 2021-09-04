6b69af8b3de952f4b553bac8f706e2c5c2a2ffde
Dalilah Aguilar <dalilahaguilar9@gmail.com>
2021-09-04 17:30:33 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+
+function distanceFromHqInBlocks (blockNumber) {
+    if (blockNumber > 42) {
+        return (blockNumber - 42);
+    } else {
+        return (42 - blockNumber)
+    }
+}
+
+function distanceFromHqInFeet(blockNumber){
+return distanceFromHqInBlocks(blockNumber) * 264
+}
+
+function distanceTravelledInFeet(blockNumber, blockNumber2){
+ if (blockNumber > 42){
+return (blockNumber-blockNumber2) * -264;
+} else {
+  return (blockNumber-blockNumber2) * 264;
+  }
+}
+function calculatesFarePrice(start, destination){
+let n = distanceTravelledInFeet(start, destination)
+if (n <= 400){
+  return 0
+} else if ( n > 400 && n < 2000 ){
+return (n - 400) * .02 
+} else if (n < 2500  && n > 2000) {
+    return 25
+} else  { 
+return 'cannot travel that far'
+} 
+}
+
+
